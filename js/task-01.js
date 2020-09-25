8a44abf5a63bd64648070c050292fa72bd263e90
IrynaBiliaieva <65736233+IrynaBiliaieva@users.noreply.github.com>
2020-09-25 09:42:36 +0300

    hw

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
 // Write code under this line
-const Account = function ({ login, email } = {}) {
+const Account = function (login, email) {
     this.login = login;
     this.email = email;
 };
-Account.prototype.getInfo = function (newAccount) {
-    console.log(`login : ${this.login}, email: ${this.email}`);
+Account.prototype.getInfo = function () {
+    let message = `'login : ${this.login}, email: ${this.email}'`;
+
+    return message;
 };
 
 console.log(typeof Account.prototype.getInfo);
@@ -17,3 +19,20 @@ console.log(mango.getInfo());
 const poly = new Account('Poly', 'poly@mail.com');
 console.log(poly.getInfo());
 // 'login : Poly, email: poly@mail.com'
+
+// const Hero = function (name, xp) {
+//     this.name = name;
+//     this.xp = xp;
+// };
+
+// Hero.prototype.gainXp = function (amount) {
+//     console.log(`${this.name} gained ${amount} experience points`);
+//     this.xp += amount;
+// };
+
+// const mango = new Hero('Mango', 1000);
+// console.log(mango); // Hero { name: 'Mango', xp: 1000 }
+
+// // Так как mango это экземпляр Hero, то ему доступны методы из Hero.prototype
+// mango.gainXp(500); // Mango gained 500 experience points
+// console.log(mango); // Hero { name: 'Mango', xp: 1500 }
